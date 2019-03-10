bfb2d2626daac170e4a3a7c1ef589ae95dec72a6
benjaminkanding <bmk1212@live.dk>
2019-03-10 17:53:28 +0100

    Retry if API request caps, now fetches highest percentile spec on boss

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var partition = sheet.getRange("web_scraping!A4").getValue();
 var healers = ["Restoration", "Holy", "Discipline", "Mistweaver"];
 var api_key = sheet.getRange("K1:M1").getValue();
 
-// Corresponding rows for importHTML in data-sheet
+// Corresponding cols for importHTML in data-sheet
 // not pretty but fast.
 var classes = {
   "Death Knight": ['A', 'B', 'C', 'D', 'E'],
@@ -30,13 +30,18 @@ var classes = {
 // should correspond to the range you want to insert data to
 var startrow = 6;
 var endrow = sheet.getRange("web_scraping!C4").getValue();
+var clearrows = 300
 
 // change to speed up execution time,
 // stops loop at 'bosses' amount of parses found.
-var bosses = sheet.getRange("LFG!R2").getValue();
-bosses = Number(bosses)
+var bosses = Number(sheet.getRange("LFG!R2").getValue());
 
 //////* END OF HEADER *//////
+
+function insertSheetmsg_(range, msg) {
+  sheet.getRange(range).setValue(msg);
+  SpreadsheetApp.flush();
+}
 
 function getData_(a) {
   var names = sheet.getRange("web_scraping!" + a[0] + startrow + ":" + a[0] + endrow).getValues();
@@ -55,35 +60,27 @@ function insertData_(names, guilds, realms, itemlvls, times) {
   sheet.getRange("E" + startrow + ":E" + endrow).setValues(realms);
   sheet.getRange("G" + startrow + ":G" + endrow).setValues(itemlvls);
   sheet.getRange("H" + startrow + ":H" + endrow).setValues(times);
-  sheet.getRange("J" + startrow + ":J" + endrow).setValue("");
-  sheet.getRange("L" + startrow + ":Y" + endrow).setValue("");
-  sheet.getRange("AA" + startrow + ":AA" + endrow).setValue("");
 }
 
 function clearData_() {
-  sheet.getRange("C" + startrow + ":C" + endrow).setValue("");
-  sheet.getRange("D" + startrow + ":D" + endrow).setValue("");
-  sheet.getRange("E" + startrow + ":E" + endrow).setValue("");
-  sheet.getRange("G" + startrow + ":G" + endrow).setValue("");
-  sheet.getRange("H" + startrow + ":H" + endrow).setValue("");
-  sheet.getRange("J" + startrow + ":J" + endrow).setValue("");
-  sheet.getRange("L" + startrow + ":Y" + endrow).setValue("");
-  sheet.getRange("AA" + startrow + ":AA" + endrow).setValue("");
-  sheet.getRange("web_scraping!C3").setValue("0");
+  sheet.getRange("C" + startrow + ":C" + clearrows).setValue("");
+  sheet.getRange("D" + startrow + ":D" + clearrows).setValue("");
+  sheet.getRange("E" + startrow + ":E" + clearrows).setValue("");
+  sheet.getRange("G" + startrow + ":G" + clearrows).setValue("");
+  sheet.getRange("H" + startrow + ":H" + clearrows).setValue("");
+  sheet.getRange("J" + startrow + ":J" + clearrows).setValue("");
+  sheet.getRange("L" + startrow + ":Y" + clearrows).setValue("");
+  sheet.getRange("AA" + startrow + ":AA" + clearrows).setValue("");
+  sheet.getRange("O3").setValue("");
 }
 
-function GetLogs_() {
-  var names = sheet.getRange("C" + startrow + ":C" + endrow).getValues();
-  var realms = sheet.getRange("E" + startrow + ":E" + endrow).getValues();
-  
-  for (var r in realms) {
-    realms[r][0] = realms[r][0].split(' ').join('-');
-    realms[r][0] = realms[r][0].split('\'').join('');
-  }
-  
+function FetchLogs_(names, realms) {
   for (var i = startrow; i <= endrow; i++) {
     var name = names[i-startrow]
     var realm = realms[i-startrow]
+    var fetch_attempts = 3
+    var fetch_wait = 8000
+    var fetching = "."
     
     if (name == "" || realm == "") {
       // empty name or realm err
@@ -91,48 +88,89 @@ function GetLogs_() {
       continue;
     }
     
-    try {
-      var response = UrlFetchApp.fetch("https://www.warcraftlogs.com:443/v1/rankings/character/" + name + "/" + realm + "/EU?partition=" + partition + "&timeframe=historical&api_key=" + api_key);
-    } catch(e) {
-      var response = false
-      // import error
-      sheet.getRange("AA" + i).setValue("IMP");
-      continue;
-    }
-    
-    if (response) {
-      var parsed = JSON.parse(response);
-      if (parsed != "" && !parsed.hidden) {
-        var spec = parsed[0]["spec"];
-        if (healers.indexOf(spec) != -1.0) {
-          response = UrlFetchApp.fetch("https://www.warcraftlogs.com:443/v1/rankings/character/" + name + "/" + realm + "/EU?partition="+ partition + "&metric=hps&timeframe=historical&api_key=" + api_key);
-          parsed = JSON.parse(response);
-        }
-        
-        // this eats up some time
-        var timestamp = sheet.getRange("A300").getValue();
-        sheet.getRange("AA" + i).setValue(timestamp);
-        sheet.getRange("J" + i).setValue(spec);
-        var datarange = [['','','','','','','','','','','','','','']];
-        
-        // keep sequential order in datarange despite holes in JSON
-        var j = 0;
-        for (var key in parsed) {
-          if (j < bosses && parsed[key]["encounterName"] && parsed[key]["difficulty"] == diff && parsed[key]["spec"] == spec) {
-            datarange[0][j] = parsed[key]["percentile"];
-            j++;
+    fetch : {
+      while (fetch_attempts > 0) {
+        try {
+          var response = UrlFetchApp.fetch("https://www.warcraftlogs.com:443/v1/rankings/character/" + name + "/" + realm + "/EU?partition=" + partition + "&timeframe=historical&api_key=" + api_key, {muteHttpExceptions: true});
+          var rcode = response.getResponseCode();
+          if (rcode != 200) { throw rcode }
+        } catch(e) {
+          Logger.log(e)
+          var response = false
+          // we only care about 'Too many requests.' error
+          if (rcode != 429) {
+            sheet.getRange("AA" + i).setValue("IMP");
+            break fetch;
+          }
+          
+          insertSheetmsg_("O3", "Too many API requests. Will resume shortly...")
+          Logger.log("Fetch failed..." + fetch_attempts)
+          
+          Utilities.sleep(fetch_wait)
+          
+          fetch_attempts -= 1
+          insertSheetmsg_("O3", "")
+          if (fetch_attempts == 0) {
+            // import error
+            sheet.getRange("AA" + i).setValue("IMP");
+            break;
           }
         }
         
-        sheet.getRange("L" + i + ":Y" + i).setValues(datarange);
-      } else if (parsed.hidden) {
-        // hidden logs
-        sheet.getRange("AA" + i).setValue("HID");
-      } else {
-        // empty json error
-        sheet.getRange("AA" + i).setValue("ERR");
+        if (response) {
+          InsertLogs_(name, realm, response, i)
+          break fetch;
+        }
       }
     }
+  }
+}
+
+
+function InsertLogs_(name, realm, response, row) {
+  var parsed = JSON.parse(response);
+  if (parsed != "" && !parsed.hidden) {
+    // this eats up some time
+    var timestamp = sheet.getRange("LFG!A300").getValue();
+    sheet.getRange("AA" + row).setValue(timestamp);
+    
+    var spec = parsed[0]["spec"];
+    var class = parsed[0]["class"];
+    if (healers.indexOf(spec) != -1.0) {
+      response = UrlFetchApp.fetch("https://www.warcraftlogs.com:443/v1/rankings/character/" + name + "/" + realm + "/EU?partition="+ partition + "&metric=hps&timeframe=historical&api_key=" + api_key);
+      parsed = JSON.parse(response);
+    }
+    
+    var datarange = [['','','','','','','','','','','','','','']];
+    var j = 0;
+    var previous = ""
+    var dirty = false
+    
+    for (var key in parsed) {
+      if (j < bosses && parsed[key]["encounterName"] && parsed[key]["difficulty"] == diff) {
+        if (parsed[key]["spec"] == spec) {
+          datarange[0][j] = parsed[key]["percentile"];
+          previous = parsed[key]["encounterName"];
+          j++;
+        } else if (parsed[key]["encounterName"] == previous && parsed[key]["percentile"] > datarange[0][j-1]) {
+          datarange[0][j-1] = parsed[key]["percentile"];
+          dirty = true;
+        }
+      }
+    }
+    
+    if (dirty) {
+      sheet.getRange("J" + row).setValue(class);
+    } else {
+      sheet.getRange("J" + row).setValue(spec);
+    }
+    sheet.getRange("L" + row + ":Y" + row).setValues(datarange);
+  } else if (parsed.hidden) {
+    // hidden logs
+    sheet.getRange("AA" + row).setValue("HID");
+  } else {
+    // empty json error
+    sheet.getRange("AA" + row).setValue("ERR");
   }
 }
 
@@ -145,13 +183,14 @@ function FillLFG() {
     return;
   }
   
-  var running = sheet.getRange("web_scraping!C3").getValue();
-  if (running == 0) {
-    sheet.getRange("web_scraping!C3").setValue("1");
-  }
+  clearData_()
+  sheet.getRange("web_scraping!C3").setValue("1");
   
   switch(request) {
-    case 0: clearData_(); return;
+    case 0: 
+      clearData_();
+      sheet.getRange("web_scraping!C3").setValue("0");
+      return;
     case 1: getData_(classes["Death Knight"]); break;
     case 2: getData_(classes["Demon Hunter"]); break;
     case 3: getData_(classes["Druid"]); break;
@@ -168,7 +207,15 @@ function FillLFG() {
     default: sheet.getRange("web_scraping!C3").setValue("0"); return;
   }
   
-  GetLogs_();
+  var names = sheet.getRange("C" + startrow + ":C" + endrow).getValues();
+  var realms = sheet.getRange("E" + startrow + ":E" + endrow).getValues();
+  
+  for (var r in realms) {
+    realms[r][0] = realms[r][0].split(' ').join('-');
+    realms[r][0] = realms[r][0].split('\'').join('');
+  }
+  
+  FetchLogs_(names, realms)
   
   sheet.getRange("web_scraping!C3").setValue("0");
   lock.releaseLock();
